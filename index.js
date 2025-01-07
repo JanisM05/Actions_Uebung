ad2f7a11384b5c0f2e8dac85fa6fbcc326401d28
JanisM05 <116655274+JanisM05@users.noreply.github.com>
2025-01-07 14:34:48 +0100

    test actions 2

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import chalk from 'chalk'
 import { generate } from 'random-words'
 // Fügen Sie hier die korrekten Imports ein. Verwenden Sie import, nicht require.
 
-const word1 = generate({ exactly: 1, minLength: 12, wordsPerString: 1 })
-const word2 = generate({ exactly: 1, minLength: 12, wordsPerString: 1 })
+const word1 = generate({ exactly: 1, minLength: 10, wordsPerString: 1 })
+const word2 = generate({ exactly: 1, minLength: 10, wordsPerString: 1 })
 console.log(`The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`)
 
 export const sum = (a, b) => {
